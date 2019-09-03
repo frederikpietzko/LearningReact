f24ee536c5dc30127ec6e9f14777b0c25fd42468
Frederik Pietzko <pietzkofred@gmail.com>
2019-09-03 21:04:39 +0200

    redux thunk

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,11 +1,47 @@
+import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPosts = async () => {
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  // this is normal way of oding things but kinda hard to read
+  const userIds = _.uniq(_.map(getState().posts, "userId"));
+  userIds.forEach(id => dispatch(fetchUser(id)));
+
+  // this is with _.chain which does the same thing but nore readabel
+  // the argument of chain will be passed down as the first argument for each chained element and modified in the process.
+  // At the end .value() has to be called so the changes actually happen
+  // _.chain(getState().props)
+  //   .map("userId")
+  //   .uniq()
+  //   .forEach(id => dispatch(fetchUser(id)))
+  //   .value();
+};
+
+export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get("/posts");
-  return {
+  dispatch({
     type: "FETCH_POSTS",
-    payload: {
-      response
-    }
-  };
+    payload: response.data
+  });
+};
+
+// memoized version
+// export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//   dispatch({
+//     type: "FETCH_USER",
+//     payload: response.data
+//   });
+// });
+
+export const fetchUser = id => async dispatch => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({
+    type: "FETCH_USER",
+    payload: response.data
+  });
 };
--- blog/src/reducers/index.js
+++ blog/src/reducers/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from "redux";
-
+import postsReducer from "./postsReducer";
+import usersReducer from "./usersReducer";
 export default combineReducers({
-  dummy: () => 10
+  posts: postsReducer,
+  users: usersReducer
 });
